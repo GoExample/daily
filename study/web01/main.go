04ff2acce1c9e71b81538f069c7b90e2a8f5a1ae
root <121753255@qq.com>
2021-10-27 17:03:16 +0800

    Tab tranfer four space

--- study/web01/main.go
+++ study/web01/main.go
@@ -1,54 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
+    "fmt"
+    "gopkg.in/ini.v1"
+    "io/ioutil"
+    "net/http"
+    "os"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	buf, _ := ioutil.ReadFile("./hello.html")
-	_, err := fmt.Fprintf(w, string(buf))
-	if err != nil {
-		return
-	}
+    buf, _ := ioutil.ReadFile("./hello.html")
+    _, err := fmt.Fprintf(w, string(buf))
+    if err != nil {
+        return
+    }
 }
 
 func main() {
-	//ff, _ := Asset("conf/conf.ini")
-	////fmt.Println(reflect.TypeOf(ff))
-	//cfg, err := ini.Load(ff)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	////fmt.Println(cfg.Section("node").Key("nodes").String())
-	//
-	//dir, _ := os.Getwd()
-	////fmt.Println(dir)
-	//path := dir + "/conf/"
-	//pathName := path + "my.ini"
-	//cfg, err = ini.Load(pathName)
-	//if err != nil {
-	//	fmt.Printf("Fail to read file: %v", err)
-	//	os.Exit(1)
-	//}
-	//b, err := modifyCfg(cfg)
-	////fmt.Println(b.String())
-	//
-	//dir, _ = os.Getwd()
-	//path = dir + "/conf/my.ini.local"
-	//
-	//f, err := os.Create(path)
-	//defer f.Close()
-	//f.WriteString(b.String())
-	http.HandleFunc("/hello", sayHello)
-	err := http.ListenAndServe(
-		":80",
-		nil,
-	)
-	if err != nil {
-		fmt.Printf("http serve failed, err:%v\n", err)
-		return
-	}
-	studyChi()
+    ff, _ := Asset("conf/conf.ini")
+    //fmt.Println(reflect.TypeOf(ff))
+    cfg, err := ini.Load(ff)
+    if err != nil {
+        fmt.Println(err)
+    }
+    //fmt.Println(cfg.Section("node").Key("nodes").String())
+
+    dir, _ := os.Getwd()
+    //fmt.Println(dir)
+    path := dir + "/conf/"
+    pathName := path + "my.ini"
+    cfg, err = ini.Load(pathName)
+    if err != nil {
+        fmt.Printf("Fail to read file: %v", err)
+        os.Exit(1)
+    }
+    b, err := modifyCfg(cfg)
+    fmt.Sprintf("%v", b.String())
+    /*
+       fmt.Println(b.String())
+       path = dir + "/conf/my.ini.local"
+       f, err := os.Create(path)
+       defer f.Close()
+       f.WriteString(b.String())
+    */
+
+    http.HandleFunc("/hello", sayHello)
+    http.ListenAndServe(
+        ":9000",
+        nil,
+    )
+    if err != nil {
+        fmt.Printf("http serve failed, err:%v\n", err)
+        return
+    }
+    studyChi()
 }
--- study/web01/studyChi.go
+++ study/web01/studyChi.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"net/http"
+    "github.com/go-chi/chi"
+    "net/http"
 )
 
 func studyChi() {
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":3000", r)
+    r := chi.NewRouter()
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("welcome"))
+    })
+    http.ListenAndServe(":3000", r)
 }

377233e5b303d7f61c9d68f3625497990db752c0
root <121753255@qq.com>
2021-10-29 02:19:34 -0700

    Add worker02 code

---
+++ concurrent/worker02/go.mod
@@ -0,0 +1,5 @@
+module worker02
+
+go 1.17
+
+require github.com/panjf2000/ants/v2 v2.4.6
---
+++ concurrent/worker02/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "fmt"
+    "github.com/panjf2000/ants/v2"
+    "os"
+    "strconv"
+    "sync"
+    "time"
+)
+
+func main() {
+    args := os.Args
+    num := 0
+    if len(args) >= 2 {
+        num, _ = strconv.Atoi(args[1])
+    }
+
+    if num == 0 {
+        num = 10
+    }
+
+    nonblocking(num)
+    blocking(num)
+}
+
+func blocking(num int) {
+    poolSize := num / 2
+    waitSize := num / 2
+    p, _ := ants.NewPool(poolSize, ants.WithMaxBlockingTasks(waitSize))
+    defer p.Release()
+
+    var wg sync.WaitGroup
+    for i := 1; i <= num; i++ {
+        wg.Add(1)
+        go func(i int) {
+            err := p.Submit(wrapper(i, &wg))
+            if err != nil {
+                fmt.Printf("task:%d err:%v\n", i, err)
+                wg.Done()
+            }
+        }(i)
+    }
+    wg.Wait()
+}
+
+func nonblocking(num int) {
+    poolSize := num / 2
+    p, _ := ants.NewPool(poolSize, ants.WithNonblocking(true))
+    defer p.Release()
+
+    var wg sync.WaitGroup
+    for i := 1; i <= num; i++ {
+        wg.Add(1)
+        err := p.Submit(wrapper(i, &wg))
+        if err != nil {
+            fmt.Printf("task:%d err:%v\n", i, err)
+            wg.Done()
+        }
+    }
+
+    wg.Wait()
+}
+
+func wrapper(i int, wg *sync.WaitGroup) func() {
+    return func() {
+        fmt.Printf("hello from task:%d\n", i)
+        time.Sleep(1 * time.Second)
+        wg.Done()
+    }
+}

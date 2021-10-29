804a01caac349cd08a142cadb588193d29a41424
root <121753255@qq.com>
2021-10-29 01:46:43 -0700

    Update worker01 code

---
+++ concurrent/worker01/go.mod
@@ -0,0 +1,3 @@
+module worker01
+
+go 1.17
--- concurrent/worker01/main.go
+++ concurrent/worker01/main.go
@@ -1,12 +1,42 @@
 package main
 
+import "C"
 import (
     "fmt"
+    "log"
+    "runtime"
     "sync"
     "time"
 )
 
+func initLog() {
+    log.SetFlags(log.Lshortfile | log.LstdFlags)
+}
+
 func main() {
+    initLog()
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
+
+    tasks := []*Task{
+        NewTask(func() error { return nil }),
+        NewTask(func() error { return nil }),
+        NewTask(func() error { return nil }),
+    }
+
+    p := NewPool(tasks, 50)
+    p.Run()
+    var numErrors int
+    for _, task := range p.Tasks {
+        if task.Err != nil {
+            log.Println(task.Err)
+            numErrors++
+        }
+        if numErrors >= 10 {
+            log.Println("Too many errors.")
+            break
+        }
+    }
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
     jobs := make(chan int, 100)
     results := make(chan int, 100)
     errors := make(chan error, 100)
@@ -15,6 +45,7 @@ func main() {
     for w := 1; w <= 3; w++ {
         go worker(w, &wg, jobs, results, errors)
     }
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
 
     for j := 1; j <= 9; j++ {
         jobs <- j
@@ -23,23 +54,28 @@ func main() {
     close(jobs)
 
     wg.Wait()
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
 
     for i := 1; i <= 100; i++ {
         select {
         case err := <-errors:
-            fmt.Println("finished with error: ", err)
+            log.Println("finished with error: ", err)
         case result := <-results:
-            fmt.Println("result is ", result)
+            log.Println("result is ", result)
         default:
         }
     }
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
+
     close(errors)
     close(results)
+    log.Printf("Have %d goroutine running\n", runtime.NumGoroutine())
+
 }
 
 func worker(id int, wg *sync.WaitGroup, jobs <-chan int, results chan<- int, errors chan<- error) {
     for j := range jobs {
-        fmt.Println("worker", id, "processing job", j)
+        log.Println("worker", id, "processing job", j)
         time.Sleep(time.Second)
         if j%2 == 0 {
             results <- j * 2
